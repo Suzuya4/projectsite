47be42626e19744c734115bb82faa5bd71be7546
Suzuya4 <tibudanjoshuaserge@gmail.com>
2023-12-13 15:34:21 +0800

    main

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import PokemonCard, Trainer, Collection
 
+
 #admin.site.register(PokemonCard)
 
 @admin.register (PokemonCard)
